3c29cda5393ea0ce7cffcb3137447e0b3d5798a1
Patrick Hulin <patrick.hulin@gmail.com>
2017-09-24 16:30:59 -0400

    Factor out common stuff.

--- otsu.py
+++ otsu.py
@@ -2,79 +2,13 @@ import cv2
 import sys
 import os
 import re
-import numpy as np
-from skimage.filters import threshold_sauvola
+
+from .lib import gradient, vert_close, text_contours, sauvola
 
 indir = sys.argv[1]
 outdir = sys.argv[2]
 
 files = filter(lambda f: re.search('.(png|jpg|tif)$', f), os.listdir(indir))
-
-cross33 = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-def gradient(im):
-    space_width = len(im) / 100 * 2 + 1
-    horiz = cv2.getStructuringElement(cv2.MORPH_RECT, (space_width, 1))
-    im = cv2.morphologyEx(im, cv2.MORPH_GRADIENT, cross33)
-    return cv2.morphologyEx(im, cv2.MORPH_CLOSE, horiz)
-
-def vert_close(im):
-    space_width = len(im) / 200 * 2 + 1
-    vert = cv2.getStructuringElement(cv2.MORPH_RECT, (1, space_width))
-    return cv2.morphologyEx(im, cv2.MORPH_CLOSE, vert)
-
-def sauvola(im):
-    thresh = threshold_sauvola(im, window_size=len(im) / 400 * 2 + 1)
-    booleans = im > (thresh * 1.0)
-    ints = booleans.astype(np.uint8) * 255
-    return ints
-
-def text_contours(im):
-    im_w, im_h = len(im[0]), len(im)
-    min_feature_size = im_h / 300
-
-    copy = im.copy()
-    cv2.rectangle(copy, (0, 0), (im_w, im_h), 255, 3)
-    contours, [hierarchy] = \
-        cv2.findContours(copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # find biggest holes
-    image_area = im_w * im_h
-    good_holes = []
-    i = 0
-    while i >= 0:
-        j = hierarchy[i][2]
-        while j >= 0:
-            c = contours[j]
-            x, y, w, h = cv2.boundingRect(c)
-            if w * h > image_area * 0.25:
-                good_holes.append(j)
-            j = hierarchy[j][0]
-        i = hierarchy[i][0]
-
-    good_contours, bad_contours = [], []
-    for hole in good_holes:
-        x, y, w, h = cv2.boundingRect(contours[hole])
-        print "hole:", x, y, w, h
-
-        i = hierarchy[hole][2]
-        while i >= 0:
-            c = contours[i]
-            x, y, w, h = cv2.boundingRect(c)
-            print 'contour:', x, y, w, h
-            if len(c) > 10 \
-                    and h < 3 * w \
-                    and w > min_feature_size \
-                    and h > min_feature_size \
-                    and x > 0.02 * im_w \
-                    and x + w < 0.98 * im_w \
-                    and y > 0.02 * im_h \
-                    and y + h < 0.98 * im_h:
-                good_contours.append(c)
-            else:
-                bad_contours.append(c)
-            i = hierarchy[i][0]
-
-    return good_contours, bad_contours
 
 def crop(im):
     im_w, im_h = len(im[0]), len(im)
--- test.py
+++ test.py
@@ -1,105 +1,40 @@
 import cv2
 import sys
-import numpy as np
 # import IPython
 import matplotlib.pyplot as plt
-from skimage.filters import threshold_sauvola
+
+from .lib import gradient, vert_close, text_contours, sauvola
 
 inpath = sys.argv[1]
 original = cv2.imread(inpath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
-def otsu(im):
-    ret, thresh = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return thresh
-
-def gradient(im):
-    space_width = len(im) / 100 * 2 + 1
-    print "gradient: close width", space_width
-    horiz = cv2.getStructuringElement(cv2.MORPH_RECT, (space_width, 1))
-    im = cv2.morphologyEx(im, cv2.MORPH_GRADIENT, cross33)
-    return cv2.morphologyEx(im, cv2.MORPH_CLOSE, horiz)
-
-def vert_close(im):
-    space_width = len(im) / 200 * 2 + 1
-    print "vert close width", space_width
-    vert = cv2.getStructuringElement(cv2.MORPH_RECT, (1, space_width))
-    return cv2.morphologyEx(im, cv2.MORPH_CLOSE, vert)
-
 def outline(im):
-    im_w, im_h = len(im[0]), len(im)
-    min_feature_size = im_h / 300
-    copy = im.copy()
-    cv2.rectangle(copy, (0, 0), (im_w, im_h), 255, 3)
-    contours, [hierarchy] = \
-        cv2.findContours(copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # find biggest hole
-    max_hole = -1
-    max_hole_area = 0
-    i = 0
-    while i >= 0:
-        j = hierarchy[i][2]
-        while j >= 0:
-            c = contours[j]
-            x, y, w, h = cv2.boundingRect(c)
-            if w * h > max_hole_area:
-                max_hole_area = w * h
-                max_hole = j
-            j = hierarchy[j][0]
-        i = hierarchy[i][0]
-
-    x, y, w, h = cv2.boundingRect(contours[max_hole])
-    print "max hole:", x, y, w, h
-
     result = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
-    i = hierarchy[max_hole][2]
-    while i >= 0:
-        c = contours[i]
+
+    good_contours, bad_contours = text_contours(im)
+
+    for c in good_contours:
         x, y, w, h = cv2.boundingRect(c)
-        # orig_slice = thresh[y:y+h, x:x+w]
-        # h_projection = (orig_slice.sum(axis=0) / im_h).astype(np.uint8)
-        if len(c) > 10 \
-                and h < 3 * w \
-                and w > min_feature_size \
-                and h > min_feature_size \
-                and x > 0.02 * im_w \
-                and x + w < 0.98 * im_w \
-                and y > 0.02 * im_h \
-                and y + h < 0.98 * im_h:
-            cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        else:
-            cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 4)
-        i = hierarchy[i][0]
+        cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    for c in bad_contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 4)
+
     return result
 
 def crop(im):
     im_w, im_h = len(im[0]), len(im)
-    min_feature_size = im_h / 300
-    copy = im.copy()
-    contours, [hierarchy] = \
-        cv2.findContours(copy, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-    i = 0
+    good_contours, _ = text_contours(im)
     crop_x0, crop_y0, crop_x1, crop_y1 = im_w, im_h, 0, 0
-    result = im.copy()
-    while i >= 0:
-        c = contours[i]
+    for c in good_contours:
         x, y, w, h = cv2.boundingRect(c)
-        if len(c) > 10 \
-                and h < 3 * w \
-                and w > min_feature_size \
-                and h > min_feature_size \
-                and x > 0.02 * im_w \
-                and x + w < 0.98 * im_w \
-                and y > 0.02 * im_h \
-                and y + h < 0.98 * im_h:
-            crop_x0 = min(x, crop_x0)
-            crop_y0 = min(y, crop_y0)
-            crop_x1 = max(x + w, crop_x1)
-            crop_y1 = max(y + h, crop_y1)
-        i = hierarchy[i][0]
+        crop_x0 = min(x, crop_x0)
+        crop_y0 = min(y, crop_y0)
+        crop_x1 = max(x + w, crop_x1)
+        crop_y1 = max(y + h, crop_y1)
 
-    result = cv2.bitwise_not(result)
+    result = cv2.bitwise_not(im)
     cv2.rectangle(result, (crop_x0, crop_y0), (crop_x1, crop_y1), 127, 4)
     crop_x0 = int(max(0, crop_x0 - .01 * im_h))
     crop_y0 = int(max(0, crop_y0 - .01 * im_h))
@@ -110,15 +45,6 @@ def crop(im):
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5, 5))
 cross33 = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
 cross55 = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
-
-def sauvola(im):
-    im = im.copy()
-    global thresh
-    thresh = threshold_sauvola(im, window_size=len(im) / 400 * 2 + 1)
-    booleans = im > (thresh * 1.0)
-    ints = booleans.astype(np.uint8) * 255
-    cv2.imwrite('sauvola.png', ints)
-    return ints
 
 transforms = [
     # ('Bilateral', lambda im: cv2.bilateralFilter(im, 5, 75, 41)),
